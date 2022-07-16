20387ce1b1b9f4586346ebad90f2fa1c0824101a
Juciano <juciano.junior@hotmail.com>
2022-07-16 23:03:51 +0100

    Adding Stack Navigation

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
 import CategoryScreen from './screens/CategoryScreen';
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
     <>
-      <CategoryScreen />
-      <StatusBar style='light'/>
+      <StatusBar style='dark'/>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoryScreen} />          
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
