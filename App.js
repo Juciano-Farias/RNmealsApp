2fd7af8ea234ebd0f9611f3f830f8d4845c3e4a0
Juciano <juciano.junior@hotmail.com>
2022-07-17 02:41:32 +0100

    Status bar

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
-import { StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import CategoryScreen from './screens/CategoryScreen';
 
 export default function App() {
   return (
-    <CategoryScreen />
+    <>
+      <CategoryScreen />
+      <StatusBar />
+    </>
   );
 }
 
