ac1a208d8ff3cc321635b65b5eb001aa53e7612d
Juciano <juciano.junior@hotmail.com>
2022-07-18 19:04:15 +0100

    feat: Header right icon

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,16 +1,30 @@
-import { Image, Text, View, StyleSheet, ScrollView } from 'react-native'
+import { useLayoutEffect } from 'react'
+import { Image, Text, View, StyleSheet, ScrollView, Button } from 'react-native'
 
 import Subtitle from '../components/MealDetail/Subtitle'
 import List from '../components/MealDetail/List'
+import IconButton from '../components/IconButton'
 
 import MealDetails from '../components/MealDetails'
 
 import { MEALS } from '../data/dummy-data'
 
-const MealDetailScreen = ({ route }) => {
+const MealDetailScreen = ({ route, navigation }) => {
   const mealId = route.params.mealId
 
   const selectedMeal = MEALS.find(meal => meal.id === mealId)
+
+  function headerButtonPressHandler() {
+    console.log('Pressed!')
+  }
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return <IconButton icon={'star'} color={'white'} onPress={headerButtonPressHandler}/>
+      }
+    })
+  },[navigation, headerButtonPressHandler])
 
   return (
     <ScrollView style={styles.rootContainer}>
