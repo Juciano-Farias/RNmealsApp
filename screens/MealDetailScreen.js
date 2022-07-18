fdd24cba73a762032f355c311abae769b444e954
Juciano <juciano.junior@hotmail.com>
2022-07-18 14:45:43 +0100

    refactor: Add List and Subtitle components and ScrollView

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,4 +1,7 @@
-import { Image, Text, View, StyleSheet } from 'react-native'
+import { Image, Text, View, StyleSheet, ScrollView } from 'react-native'
+
+import Subtitle from '../components/MealDetail/Subtitle'
+import List from '../components/MealDetail/List'
 
 import MealDetails from '../components/MealDetails'
 
@@ -10,37 +13,51 @@ const MealDetailScreen = ({ route }) => {
   const selectedMeal = MEALS.find(meal => meal.id === mealId)
 
   return (
-    <View>
-      <Image source={{ uri: selectedMeal.imageUrl }}/>
-      <Text>{selectedMeal.title}</Text>
+    <ScrollView style={styles.rootContainer}>
+      <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }}/>
+      <Text style={styles.title} >{selectedMeal.title}</Text>
       <MealDetails
         duration={selectedMeal.duration}
         complexity={selectedMeal.complexity}
         affordability={selectedMeal.affordability}
+        textStyle={styles.detailText}
       />
-      <Text>Ingredient</Text>
-      {selectedMeal.ingredients.map(ingredient => (
-        <Text key={ingredient}>{ingredient}</Text>
-      ))}
-      <Text>Steps</Text>
-      {selectedMeal.steps.map(step => (
-        <Text key={step}>{step}</Text>
-      ))}
-    </View>
+      <View style={styles.listOutterContainer}>
+        <View style={styles.listContainer }>
+          <Subtitle>Ingredients</Subtitle>
+          <List data={selectedMeal.ingredients}/>
+          <Subtitle>Steps</Subtitle>
+          <List data={selectedMeal.steps}/>
+        </View>
+      </View>
+    </ScrollView>
   )
 }
 
 export default MealDetailScreen
 
 const styles = StyleSheet.create({
-  details: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 8,
+  rootContainer: {
+    marginBottom: 32,
   },
-  detailItem: {
-    marginHorizontal: 4,
-    fontSize: 12,
+  image: {
+    width: '100%',
+    height: 300,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    margin: 8,
+    textAlign: 'center',
+    color: 'white',
+  },
+  detailText: {
+    color: 'white',
+  },
+  listOutterContainer: {
+    alignItems: 'center',
+  },
+  listContainer: {
+    maxWidth: '80%',
   },
 })
