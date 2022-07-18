424630a1bfef57380e919d3384384b4c5191483c
Juciano <juciano.junior@hotmail.com>
2022-07-18 14:04:47 +0100

    feat: All MealDetailScreen's data

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,13 +1,46 @@
-import { useLayoutEffect } from 'react'
-import { Text } from 'react-native'
+import { Image, Text, View, StyleSheet } from 'react-native'
+
+import MealDetails from '../components/MealDetails'
 
 import { MEALS } from '../data/dummy-data'
 
 const MealDetailScreen = ({ route }) => {
+  const mealId = route.params.mealId
+
+  const selectedMeal = MEALS.find(meal => meal.id === mealId)
 
   return (
-    <Text>{route.params.mealId}</Text>
+    <View>
+      <Image source={{ uri: selectedMeal.imageUrl }}/>
+      <Text>{selectedMeal.title}</Text>
+      <MealDetails
+        duration={selectedMeal.duration}
+        complexity={selectedMeal.complexity}
+        affordability={selectedMeal.affordability}
+      />
+      <Text>Ingredient</Text>
+      {selectedMeal.ingredients.map(ingredient => (
+        <Text key={ingredient}>{ingredient}</Text>
+      ))}
+      <Text>Steps</Text>
+      {selectedMeal.steps.map(step => (
+        <Text key={step}>{step}</Text>
+      ))}
+    </View>
   )
 }
 
 export default MealDetailScreen
+
+const styles = StyleSheet.create({
+  details: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 8,
+  },
+  detailItem: {
+    marginHorizontal: 4,
+    fontSize: 12,
+  },
+})
