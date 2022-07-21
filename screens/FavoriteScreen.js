60e7d21a336a04f612a41053b20d3525bed87bb6
Juciano <juciano.junior@hotmail.com>
2022-07-21 19:03:16 +0100

    feat: useContext hook to manage the favorite meals

--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -1,8 +1,15 @@
-import { Text } from "react-native"
+import { useContext } from 'react'
+import { FavoritesContext } from '../store/context/favorite-context'
+import { MealsList } from '../components/MealsList/MealsList'
+import { MEALS } from '../data/dummy-data'
 
 const FavoriteScreen = () => {
+  const favoriteMealsContext = useContext(FavoritesContext)
+
+  const favoriteMeals = MEALS.filter(meal => favoriteMealsContext.ids.includes(meal))
+
   return (
-    <Text>The favorite Screen</Text>
+    <MealsList items={favoriteMeals}/>
   )
 }
 
